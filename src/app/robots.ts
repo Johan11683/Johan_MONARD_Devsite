adb565304da3c8f91cb109a670ba05710f8a1011
Johan MONARD <monard.johan@yahoo.fr>
2025-12-03 11:11:24 +0100

    ajout sitemap et robots pour google console

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+export default function robots() {
+  const baseUrl = "https://devhook.fr";
+
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  };
+}
